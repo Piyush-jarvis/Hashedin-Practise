3cd1f0430068278eb51c85640d13a9c271115d76
Piyush <asawapiyush20@gmail.com>
2023-01-01 06:57:40 +0530

    Build Tree and Level Order Traversal of Tree imp + inoreder+ postorder + preorder gtraversal

--- Build_Tree.cpp
+++ Build_Tree.cpp
@@ -49,7 +49,7 @@ void LevelOrderTraversal(Node* root)
 {
     queue<Node*> q;
     q.push(root);
-    q.push(NULL);//separator
+    q.push(NULL);//separator if u want it to be in tree structure than use separator
 
     while (!q.empty())
     {
@@ -78,6 +78,38 @@ void LevelOrderTraversal(Node* root)
     }  
 }
 
+void inorderTraversal(Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    inorderTraversal(root->left);
+    cout << root->data <<" ";
+    inorderTraversal(root->right);
+}
+
+void postorderTraversal (Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    cout << root->data <<" ";
+}
+
+void preorderTraversal(Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    cout << root->data <<" ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
 
 int main(){
     Node* root = NULL;
@@ -86,5 +118,11 @@ int main(){
     // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
     cout<<"Printing the LevelOrder Traversal of the Tree: "<< endl;
     LevelOrderTraversal(root);
+    cout <<"Inorder Traversal is :";
+    inorderTraversal(root);
+    cout << endl <<"Postorder Traversal is :";
+    postorderTraversal(root);
+    cout << endl <<"Preorder Traversal is :";
+    preorderTraversal(root);
     return 0;
 }
